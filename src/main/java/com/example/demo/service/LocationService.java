65492f48dfbd026c7a77bb862678ceeea5abfdbd
WoldemarK <kovtynov.vladimir@gmail.com>
2022-12-16 01:00:06 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/service/LocationService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.LocationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LocationService {
+    private final LocationRepository locationRepository;
+}
